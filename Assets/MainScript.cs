7815b48dad0a2fac6c3e1bd17d3e72c1d2eb2410
Nicolas Vidal <pro@nicolasvidal.fr>
2019-10-30 16:15:41 +0100

    Cubes Scene is setup ! :)

---
+++ Assets/MainScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] cubes;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Hello les 3A IABD1 !!! :)");
+        Debug.Log($"Il y a {cubes.Length} cubes dans la scene !!! :)");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
