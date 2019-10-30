db33c6c9c19ec7bec3d70f9e7576553934cacf5b
Nicolas Vidal <pro@nicolasvidal.fr>
2019-10-30 16:54:52 +0100

    Asyncrhonous Equipropable Efficient Random seems to be working ! :)

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MainScript : MonoBehaviour
 {
@@ -10,10 +11,23 @@ public class MainScript : MonoBehaviour
     {
         Debug.Log("Hello les 3A IABD1 !!! :)");
         Debug.Log($"Il y a {cubes.Length} cubes dans la scene !!! :)");
+        StartCoroutine(ShuffleCubePositions(cubes));
     }
 
-    // Update is called once per frame
-    private void Update()
+    private static IEnumerator ShuffleCubePositions(Transform[] cubes)
     {
+        for (var i = 0; i < cubes.Length; i++)
+        {
+            var rdmIdx = Random.Range(i, cubes.Length);
+            SwapPositions(cubes[i], cubes[rdmIdx]);
+            yield return null;
+        }
+    }
+
+    private static void SwapPositions(Transform cube1, Transform cube2)
+    {
+        var tmp = cube1.position;
+        cube1.position = cube2.position;
+        cube2.position = tmp;
     }
 }
